085e607375dd2e32c27fb75de42e5a76d4a4513a
esaddag <dagmuhammedesad@gmail.com>
2020-09-09 01:00:07 +0300

    changes related to path

--- face_detector/views.py
+++ face_detector/views.py
@@ -16,76 +16,8 @@ import base64
 FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_default.xml".format(
 	base_path=os.path.abspath(os.path.dirname(__file__)))
 
-
-class MyIndexView(generic.ListView):
+class MyIndexView(generic.View):
 	template_name = "face_detector/index.html"
-	def get(self, request):
-		base64.b64decode(request.FILES["image"])
-		print(type(image))
-	def get_queryset(self):
-		return DetectedFace.objects.all()
-
-
-
-class ResultView(generic.ListView):
-	
-	def get(self, request):
-		print(type(request.FILES["image"]))
-	
-	def get_queryset(self):
-		return DetectedFace.objects.all()
-
-
-
-
-# find faces and returns as json
-@csrf_exempt
-def detect_with_post(req):
-	
-	# initialize the data dictionary to be returned by the request
-	data = {"success": False}
-	
-	# check to see if this is a post request
-	if req.method == "POST":
-		
-		# check to see if an image was uploaded 
-		if req.FILES.get("image", None) is not None:
-			
-			# grab the uploaded image
-			image = _grab_image(stream=req.FILES["image"])
-		
-		# otherwise, assume that a URL was passed in
-		else:
-			
-			# grab the URL from the request
-			url = req.POST.get("url", None)
-			
-			# if the URL is None, then return an error
-			if url is None:
-				data["error"] = "No URL provided."
-				return JsonResponse(data)
-			
-			# load the image and convert
-			image = _grab_image(url=url)
-		
-		# convert the image to grayscale
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		
-		# load the face cascade detector
-		detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
-		
-		# and detect faces in the image
-		rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5,
-			minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
-		
-		# construct a list of bounding boxes from the detection
-		rects = [(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in rects]
-		
-		# update the data dictionary with the faces detected
-		data.update({"num_faces": len(rects), "faces": rects, "success": True})
-	
-	# return a JSON response
-	return JsonResponse(data)
 
 # find faces and draw it on the image
 @csrf_exempt
@@ -173,3 +105,53 @@ def _grab_image(path=None, stream=None, url=None):
  
 	# return the image
 	return image
+
+
+# find faces and returns as json
+@csrf_exempt
+def detect_with_post(req):
+	
+	# initialize the data dictionary to be returned by the request
+	data = {"success": False}
+	
+	# check to see if this is a post request
+	if req.method == "POST":
+		
+		# check to see if an image was uploaded 
+		if req.FILES.get("image", None) is not None:
+			
+			# grab the uploaded image
+			image = _grab_image(stream=req.FILES["image"])
+		
+		# otherwise, assume that a URL was passed in
+		else:
+			
+			# grab the URL from the request
+			url = req.POST.get("url", None)
+			
+			# if the URL is None, then return an error
+			if url is None:
+				data["error"] = "No URL provided."
+				return JsonResponse(data)
+			
+			# load the image and convert
+			image = _grab_image(url=url)
+		
+		# convert the image to grayscale
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		
+		# load the face cascade detector
+		detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
+		
+		# and detect faces in the image
+		rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5,
+			minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+		
+		# construct a list of bounding boxes from the detection
+		rects = [(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in rects]
+		
+		# update the data dictionary with the faces detected
+		data.update({"num_faces": len(rects), "faces": rects, "success": True})
+	
+	# return a JSON response
+	return JsonResponse(data)
